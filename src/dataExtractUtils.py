933638b41763662a7ab7b0d909ebe554676b1e25
Jim Swainston <jimswainston@gmail.com>
2022-09-02 13:54:26 +0100

    Added article entity extraction feature

--- src/dataExtractUtils.py
+++ src/dataExtractUtils.py
@@ -1,4 +1,7 @@
 import constants
+import uuid
+from numpy import nan as NA
+import datetime 
 
 def matchCountriesInAffiliation(affiliation):
     matches = []
@@ -9,3 +12,56 @@ def matchCountriesInAffiliation(affiliation):
     if len(matches) > 0:
         return matches
     
+
+
+def processPublishedDate(jsonPublishedObject):
+    dateParts = jsonPublishedObject["date-parts"][0]
+    if len(dateParts) == 0:
+        return NA
+    elif len(dateParts) == 3:
+        year = dateParts[0]
+        month = dateParts[1]
+        day = dateParts[2]
+        publishedDate = datetime.datetime(year,month,day)
+        return publishedDate
+    elif len(dateParts) == 2:
+        year = dateParts[0]
+        month = dateParts[1]
+        day = 1 # if not available assume first day of month
+        publishedDate = datetime.datetime(year,month,day)
+        return publishedDate
+    else:
+        return NA
+
+
+def processArticle(article):
+    # create unique id for article
+    article_id = uuid.uuid4()
+
+    if "DOI" in article:
+        DOI = article["DOI"]
+    else:
+        DOI = NA
+
+    if "title" in article:
+        title = article["title"]
+    else:
+        title = NA
+
+    if "published" in article:
+        publishedDate = processPublishedDate(article["published"])
+    else:
+        publishedDate = NA
+
+    return [article_id,DOI,title,publishedDate]
+
+
+
+
+
+
+
+
+
+
+

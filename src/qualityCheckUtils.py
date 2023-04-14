0299134436b4c49fc3a825712e2e265639f3e1a0
Jim Swainston <jimswainston@gmail.com>
2023-04-14 13:41:23 +0100

    Created quality check module with a primary key test

---
+++ src/qualityCheckUtils.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import warnings
+import pandas.io.sql as sqlio
+import psycopg2
+from psycopg2 import sql
+
+def primary_key_count_test(connection,table_name,primary_key):
+    """
+    Test to see if a database table has the same number of rows as unique primary keys. 
+    Use this to check if every row in your table has a unique primary key.
+    
+    Parameters
+    ----------
+    connection  : a psycopg2 connection object
+        Handles the connection to a PostgreSQL database instance
+    table_name  : the name of the table being tested
+    primary_key : the column name that contains the primary key for a record
+    
+    """
+
+    conn = connection
+
+    primary_key_check = sql.SQL("""select 
+    count ({primary_key}) as total_rows, count (distinct {primary_key}) as unique_primary_keys
+    from {table_name}
+    """).format(table_name = sql.Identifier(table_name),
+    primary_key = sql.Identifier(primary_key),)
+
+    #use to debug issues with query composition
+    #print(primary_key_check.as_string(conn))
+
+    result = sqlio.read_sql_query(primary_key_check, conn)
+    total_rows = result.iloc[0]['total_rows']
+    total_primary_keys = result.iloc[0]['total_rows']
+    
+    if total_rows == total_primary_keys:
+        print("Primary key check passed for table " + table_name + "\n")
+        print("total_rows is " + str(total_rows) +"\n")
+        print("total_primary_keys is " + str(total_rows))
+    else:
+        print("Primary Key Check failed for table" + table_name +  " Each row does not have a unique primary key\n")
+        print("total_rows is " + str(total_rows) +  "\n")
+        print("total_primary_keys is " + str(total_rows))
+        
+
+    
+    
